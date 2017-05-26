bbdc51f688c3f6954fa5c670c03bff253d3308c4
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-05-26 15:42:54 -0700

    added console and position setting

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ winningPositions = [[[0, 0], [0, 1], [0, 2]], [[0, 0], [1, 0], [2, 0]],
                     [[2, 0], [1, 1], [0, 2]], [[2, 0], [1, 1], [0, 2]]]
 
 gameBoard = []
+gameWon = False
 
 
 def generateBoard():
@@ -14,12 +15,22 @@ def generateBoard():
 
 def printBoard():
     for row in gameBoard:
-        print(
-            " ".join(row))
+        print(" ".join(row))
+
+
+
+def setPosition(currentrow, currentcol, playername):
+    gameBoard[currentrow][currentcol] = playername
 
 
 if __name__ == '__main__':
+    playerName = input("Please enter Name: ")
+    print("Your name will be referred to as :", playerName[0])
+    print("The following is the board: ")
     generateBoard()
     printBoard()
-    #     var = input("Please enter something: ")
-    #     print("you entered", var)
+    print("Where do you want the move to be? ")
+    row = int(input("Enter row: ")) - 1
+    col = int(input("Enter col: ")) - 1
+    setPosition(row, col, playerName[0])
+    printBoard()
